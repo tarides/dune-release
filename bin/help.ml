6ce796211aa284be2b329d723127ada991ffdcae
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-04-12 18:44:50 +0200

    Review the manual a bit

--- bin/help.ml
+++ bin/help.ml
@@ -4,7 +4,7 @@
    %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
-let topkg_manual = "Topkg manual"
+let dune_release_manual = "dune-release manual"
 let version = "%%VERSION%%"
 
 (* Help manuals *)
@@ -12,78 +12,80 @@ let version = "%%VERSION%%"
 open Cmdliner
 
 let see_also ~cmds =
-  let cmds = (Astring.String.concat ~sep:"(1), " ("topkg" :: cmds)) ^ "(1)" in
+  let cmds =
+    (Astring.String.concat ~sep:"(1), " ("dune-release" :: cmds)) ^ "(1)"
+  in
   [ `S Manpage.s_see_also; `P cmds ]
 
 let release =
-  ("TOPKG-RELEASE", 7, "", version, topkg_manual),
+  ("DUNE-RELEASE", 7, "", version, dune_release_manual),
   [ `S Manpage.s_name;
-    `P "topkg-release - How to release a (topkg) package";
+    `P "dune-release - How to release a (dune) package";
     `S Manpage.s_description;
     `P "The basic release script is the following. Each step is
         refined and explained with more details below.";
     `Pre "\
-topkg browse issues # Review remaining outstanding issues
-topkg status        # Review the changes since last version
-topkg log edit      # Write the release notes
-topkg log commit    # Commit the release notes
-topkg tag           # Tag the distribution with a version
-topkg distrib       # Create the distribution archive
-topkg publish       # Publish it on the WWW with its documentation
-topkg opam pkg      # Create an opam package
-topkg opam submit   # Submit it to OCaml's opam repository";
+dune-release browse issues # Review remaining outstanding issues
+dune-release status        # Review the changes since last version
+dune-release log edit      # Write the release notes
+dune-release log commit    # Commit the release notes
+dune-release tag           # Tag the distribution with a version
+dune-release distrib       # Create the distribution archive
+dune-release publish       # Publish it on the WWW with its documentation
+dune-release opam pkg      # Create an opam package
+dune-release opam submit   # Submit it to OCaml's opam repository";
     `P "The last four steps can be performed via a single invocation
-        to topkg-bistro(1).";
+        to dune-release-bistro(1).";
     `S "BASIC CHECKS";
     `P "First have a look at the outstanding issues the package may have
         by checking the issue tracker.";
-    `Pre "topkg browse issues";
+    `Pre "dune-release browse issues";
     `P "If the package's delegate supports issue tracker interaction
-        (see topkg-delegate(7)), you can consult them directly in the
+        (see dune-release-delegate(7)), you can consult them directly in the
         terminal with:";
-    `Pre "topkg issue list";
+    `Pre "dune-release issue list";
     `P "Basic checks are performed on the distribution archive when it is
         created, but save time by catching errors early. Hence test that
         your source repository lints and that it builds in the current build
         environment and that the package tests pass.";
     `Pre "\
-topkg lint
-topkg build # Check out the generated opam install file too
-topkg test";
+dune-release lint
+jbuilder build # Check out the generated opam install file too
+jbuilder runtest";
     `S "WRITE THE RELEASE NOTES";
     `P "Carefully write the release notes in the package's change log, these
         are essential time savers for users of the package. It may help to
         consult the list of changes that were committed since the last VCS
         version tag with:";
-    `Pre "topkg status";
+    `Pre "dune-release status";
     `P "You can then write the release notes and commit them to the VCS with:";
     `Pre "\
-topkg log edit
-topkg log commit";
+dune-release log edit
+dune-release log commit";
     `P "The next step is simplified if the change log follows a certain
-        format, see topkg-log(1) for details.";
+        format, see dune-release-log(1) for details.";
     `P "The last two commands mentioned perform no magic, it is entirely up
         to you to use them or not. The first one simply opens the change log
         of the package in your \\$EDITOR and the second one commits it to
         your VCS with a dull, canned, commit message.";
     `S "VCS TAG THE RELEASE";
-    `P "Here again topkg provides a magic-less command that will simply
+    `P "Here again dune-release provides a magic-less command that will simply
         extract the latest version tag from the package's change log
         and tag the VCS HEAD commit with it:";
-    `Pre "topkg tag";
+    `Pre "dune-release tag";
     `P "This will only work if the change log follows a certain format,
         see tokpg-log(1) for details. You can check the extracted tag is
         the one you wish before with:";
-    `Pre "topkg log -t";
-    `P "If you do not want to rely on topkg's broken extraction algorithms
+    `Pre "dune-release log -t";
+    `P "If you do not want to rely on dune-release's broken extraction algorithms
         just specify it on the command line:";
-    `Pre "topkg tag v1.0.1";
-    `P "And if you really think topkg does a bad job at this, simply
+    `Pre "dune-release tag v1.0.1";
+    `P "And if you really think dune-release does a bad job at this, simply
         use your VCS directly to tag a release.";
     `S "CREATE THE DISTRIBUTION ARCHIVE AND PUBLISH IT";
     `P "Now that the release is tagged in your VCS, generate a distribution
         archive for it in the build directory with:";
-    `Pre "topkg distrib";
+    `Pre "dune-release distrib";
     `P "This uses the source tree of the HEAD commit for creating a
         distribution in the build directory. The distribution version
         string is the VCS tag description (e.g.  git-describe(1)) of
@@ -91,9 +93,9 @@ topkg log commit";
         line.";
     `P "If everything went well you can now publish the distribution and
         its documentation on the WWW. The exact actions that happen here
-        depend on the package's delegate, see topkg-delegate(7) for more
+        depend on the package's delegate, see dune-release-delegate(7) for more
         details.";
-    `Pre "topkg publish";
+    `Pre "dune-release publish";
     `P "The distribution is now public. It may already have been picked up
         by other systems hence do not try to alter the archive and
         republish it with a different bit-stream after that point (if
@@ -106,25 +108,25 @@ topkg log commit";
     `P "The following steps still need the distribution archive created in
         the preceeding step to be in the build directory. If that's no
         longer the case but nothing moved in your VCS, you can simply
-        invoke $(b,topkg distrib), it should produce a bit-wise identical
+        invoke $(b,dune-release distrib), it should produce a bit-wise identical
         archive. If the VCS moved checkout the distribution commit to
         regenerate the archive or provide, in the subsequent commands,
         the archive manually via the $(b,--dist-file) option, see
-        topkg-opam(1) for details.";
+        dune-release-opam(1) for details.";
     `P "To add the package to OCaml's opam repository, start by creating an
         opam package description in the build directory with:";
-    `Pre "topkg opam pkg";
+    `Pre "dune-release opam pkg";
     `P "then simply submit it to the opam repository with:";
-    `Pre "topkg opam submit";
+    `Pre "dune-release opam submit";
     `P "The latter does nothing more than invoking opam-publish-submit(1) on
         the package description generated earlier.";
     `P "Congratulations. You are done. Ditch your computer.";
     `S "TROUBLESHOOTING";
     `P "Here are a few troubleshooting scenarios and possible resolution.";
     `I ("Before publishing",
-        "Anything that happens before the $(b,topkg publish) step,
-         like a failing $(b,topkg distrib), is easy to resolve. Delete the
-         version tag of your VCS, a $(b,topkg tag -d) will do, add
+        "Anything that happens before the $(b,dune-release publish) step,
+         like a failing $(b,dune-release distrib), is easy to resolve. Delete the
+         version tag of your VCS, a $(b,dune-release tag -d) will do, add
          some commits, adjust your release notes and start over.");
     `I ("opam submission build failure",
         "If the build failure is due to a missing constraint, follow the
@@ -137,11 +139,11 @@ topkg log commit";
         "These pesky but loved maintainers and robots... If they
          complain about certain aspects of your opam submission, you can either
          try to correct it manually from the opam package description found
-         in the build directory and reinvoke $(b,topkg opam submit) or edit
+         in the build directory and reinvoke $(b,dune-release opam submit) or edit
          the opam file of the source repository and regenerate the opam Package
-         description with $(b,topkg opam pkg) and the $(b,--pkg-opam)
+         description with $(b,dune-release opam pkg) and the $(b,--pkg-opam)
          option. Note that if the VCS moved meanwhile you may have to use
-         the various command line options of topkg-opam(1) to make sure
+         the various command line options of dune-release-opam(1) to make sure
          you point to the right package version and distribution archive.
          In either case you should be aware that there will be a mismatch
          between the opam file in the distribution archive and the one
@@ -150,16 +152,16 @@ topkg log commit";
     `Blocks (see_also ~cmds:[]); ]
 
 let delegate =
-  ("TOPKG-DELEGATE", 7, "", version, topkg_manual),
+  ("DUNE-RELEASE-DELEGATE", 7, "", version, dune_release_manual),
   [ `S Manpage.s_name;
-    `P "topkg-delegate - The topkg delegate";
+    `P "dune-release-delegate - The dune-release delegate";
     `S Manpage.s_description;
-    `P "The delegate of a package is a program invoked by topkg to perform
+    `P "The delegate of a package is a program invoked by dune-release to perform
         actions that are difficult or not desirable to standardize within
-        topkg itself, namely:";
-    `I ("$(b,topkg publish)",
+        dune-release itself, namely:";
+    `I ("$(b,dune-release publish)",
         "Publish distribution archives and derived artefacts.");
-    `I ("$(b,topkg issue)", "Interact with the package's issue tracker.");
+    `I ("$(b,dune-release issue)", "Interact with the package's issue tracker.");
     `P "A sample delegate is provided at the end of this man page.";
     `S "DELEGATE LOOKUP PROCEDURE";
     `P "The delegate used by a package is defined by the first match in the
@@ -167,34 +169,34 @@ let delegate =
     `I ("1. Command line", "Specified with the $(b,--delegate) option on
          the command line.");
     `I ("2. Package description.", "Specified in the package description file,
-         see topkg's API documentation.");
-    `I ("3. Environment variable.", "Specified in the TOPKG_DELEGATE
+         see dune-release's API documentation.");
+    `I ("3. Environment variable.", "Specified in the DUNE_RELEASE_DELEGATE
          environment variable.");
     `I ("4. Homepage derived discovery.", "Consult the 'homepage' field of the
         package's opam file, extract the second-level domain of the URI as
-        \\$NAME and uses the tool $(b,\\$NAME-topkg-delegate) iff it exists
+        \\$NAME and uses the tool $(b,\\$NAME-dune-release-delegate) iff it exists
         in the executable search path. For example if the homepage is
         http://www.example.org/mypackage, an existing
-        $(b,example-topkg-delegate) tool will be used.");
+        $(b,example-dune-release-delegate) tool will be used.");
     `I ("5. Transitory toy github fallback.", "If the previous step yields
-         $(b,github-topkg-delegate) but that it doesn't exist in the
-         executable search path. The $(b,toy-github-topkg-delegate) tool
-         distributed with topkg-care is used. This tool will disappear in
+         $(b,github-dune-release-delegate) but that it doesn't exist in the
+         executable search path. The $(b,toy-github-dune-release-delegate) tool
+         distributed with dune-release is used. This tool will disappear in
          the future whenever a good github delegate emerges. Consult
-         $(b,toy-github-topkg-delegate --help) for more information.");
+         $(b,toy-github-dune-release-delegate --help) for more information.");
     `S "DELEGATE PROTOCOL";
-    `P "The delegate is invoked by $(b,topkg) with a request in order to
+    `P "The delegate is invoked by $(b,dune-release) with a request in order to
         finish its own execution. This means that the delegate takes over
-        $(b,topkg)'s standard channels and is in charge until the end of
+        $(b,dune-release)'s standard channels and is in charge until the end of
         execution (except on errors, see below).";
     `P "The delegate always gets information as command line arguments with
         file paths arguments given as absolute paths. The first argument is
         always 'ipc' and is followed by a verbosity parameter:";
-    `P "my-topkg-delegate ipc $(i,VERB) $(i,ARG) ...";
+    `P "my-dune-release-delegate ipc $(i,VERB) $(i,ARG) ...";
     `P "$(i,VERB) will be either `quiet', `error', `warning', `info' or
         `debug' and the delegate must adjust its logging level appropriately.
         The remaining arguments are the request, see below for requests
-        made by $(b,topkg).";
+        made by $(b,dune-release).";
     `P "The delegate must always exit with one of the following exit codes:";
     `I ("0", "The request is successful."); `Noblank;
     `I ("1", "The request is unsupported."); `Noblank;
@@ -208,7 +210,7 @@ let delegate =
         according to its semantics. In this case it is the delegate's duty
         to provide good error messages for diagnosis on standard output";
     `P "In both non-zero exit codes, it is not the delegate's duty to
-        try to save request data. In these cases $(b,topkg) will take over
+        try to save request data. In these cases $(b,dune-release) will take over
         again in order to prevent user input data loss. This
         occurs for example on issue creation, so that the issue
         description the user may have input interactively is not
@@ -221,7 +223,7 @@ let delegate =
          $(i,MSG) $(i,ARCHIVE)",
         "Publish the distribution archive file $(i,ARCHIVE) for the package
          named $(i,NAME) at version $(i,VERSION) with publication
-         message $(i,MSG). See topkg API's documentation
+         message $(i,MSG). See dune-release API's documentation
          for information about the value of $(i,DISTRIB_URI).");
     `I ("publish doc $(i,DOC_URI) $(i,NAME) $(i,VERSION) $(i,MSG) $(i,DOCDIR)",
         "Publish the documentation directory $(i,DOCDIR) for the package
@@ -299,21 +301,21 @@ let main () =
 
 let () = exit (main ())
 ";
-    `Blocks (see_also ~cmds:["topkg-issue"; "topkg-publish"]); ]
+    `Blocks (see_also ~cmds:["dune-release-issue"; "dune-release-publish"]); ]
 
 let troubleshoot =
-  ("TOPKG-TROUBLESHOOT", 7, "", version, topkg_manual),
+  ("DUNE-RELEASE-TROUBLESHOOT", 7, "", version, dune_release_manual),
   [ `S Manpage.s_name;
-    `P "topkg-troubleshoot - A few troubleshooting tips";
+    `P "dune-release-troubleshoot - A few troubleshooting tips";
     `S Manpage.s_description;
     `P "If you get into trouble try the following to get a better undersanding
         of what is happening.";
     `S "ASK FOR MORE LOGGING";
-    `P "Invoke $(b,topkg) with $(b,-v), $(b,-v -v), or use the
-        TOPKG_VERBOSITY environment variable; see the $(b,--verbosity)
+    `P "Invoke $(b,dune-release) with $(b,-v), $(b,-v -v), or use the
+        DUNE_RELEASE_VERBOSITY environment variable; see the $(b,--verbosity)
         option.";
-    `P "Messages comming from the $(b,topkg) tool are prefixed
-        by 'topkg:' while those comming from the package description are
+    `P "Messages comming from the $(b,dune-release) tool are prefixed
+        by 'dune-release:' while those comming from the package description are
         prefixed by its base name, usually 'pkg.ml:'.";
     `S "DEBUG THE GENERATED OPAM INSTALL FILE";
     `P "To debug the generated opam install file according to the build
@@ -321,14 +323,14 @@ let troubleshoot =
         $(b,--dry-run) (or $(b,-d)) option and add a little bit of logging to
         output the build configuration that was determined:";
     `Pre "pkg/pkg.ml build -d -v [OPTION]...";`Noblank;
-    `Pre "topkg build -d -v [OPTION]...      # mostly equivalent";
+    `Pre "dune-release build -d -v [OPTION]...      # mostly equivalent";
     `S "DEBUG DEV PACKAGE INSTALLS";
      `P "If you need more information about what happens when dev packages
          are installed (VCS pins or VCS packages) in opam, for example the
          actual watermark values, invoke opam as follows:";
-    `P "TOPKG_VERBOSITY=debug opam upgrade mypkg -v";
+    `P "DUNE_RELEASE_VERBOSITY=debug opam upgrade mypkg -v";
     `S "RELEASE PROCESS TROUBLES";
-    `P "See the TROUBLESHOOTING section of topkg-release(7).";
+    `P "See the TROUBLESHOOTING section of dune-release(7).";
     `Blocks (see_also ~cmds:[]) ]
 
 (* Help command *)
@@ -363,7 +365,7 @@ let topic =
   let doc = "The topic to get help on, `topics' lists the topic." in
   Arg.(value & pos 0 (some string) None & info [] ~docv:"TOPIC" ~doc)
 
-let doc = "Show help about topkg"
+let doc = "Show help about dune-release"
 let sdocs = Manpage.s_common_options
 let exits = Cli.exits
 let man_xrefs = [`Main]
