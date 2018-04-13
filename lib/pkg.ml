e6b9728261796b83ac2baf0d66db7bb4af00c0a7
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-04-13 09:40:38 +0200

    Move code around to put all the lint functions closer

--- lib/pkg.ml
+++ lib/pkg.ml
@@ -171,6 +171,58 @@ let distrib_file p = match p.distrib_file with
     |> R.reword_error_msg
       (fun _ -> R.msgf "Did you forget to call 'dune-release distrib' ?")
 
+
+let distrib_owner_and_repo p =
+  distrib_uri p >>= fun uri ->
+  let uri_error uri =
+    R.msgf "Could not derive owner and repo from opam dev-repo \
+            field value %a; expected the pattern \
+            $SCHEME://$HOST/$OWNER/$REPO[.$EXT][/$DIR]" String.dump uri
+  in
+  match Text.split_uri ~rel:true uri with
+  | None -> Error (uri_error uri)
+  | Some (_, _, path) ->
+      if path = "" then Error (uri_error uri) else
+      match String.cut ~sep:"/" path with
+      | None -> Error (uri_error uri)
+      | Some (owner, path) ->
+          let repo = match String.cut ~sep:"/" path with
+          | None -> path
+          | Some (repo, _) -> repo
+          in
+          begin
+            Fpath.of_string repo
+            >>= fun repo -> Ok (owner, Fpath.(to_string @@ rem_ext repo))
+          end
+          |> R.reword_error_msg (fun _ -> uri_error uri)
+
+let doc_uri p = opam_field_hd p "doc" >>| function
+  | None     -> ""
+  | Some uri -> uri
+
+let doc_owner_repo_and_path p =
+  doc_uri p >>= fun uri ->
+  (* Parses the $PATH of $SCHEME://$HOST/$REPO/$PATH *)
+  let uri_error uri =
+    R.msgf "Could not derive publication directory $PATH from opam doc \
+            field value %a; expected the pattern \
+            $SCHEME://$OWNER.github.io/$REPO/$PATH" String.dump uri
+  in
+  match Text.split_uri ~rel:true uri with
+  | None -> Error (uri_error uri)
+  | Some (_, host, path) ->
+      if path = "" then Error (uri_error uri) else
+      (match String.cut ~sep:"." host with
+      | Some (owner, g) when String.equal g "github.io" -> Ok owner
+      | _ -> Error (uri_error uri))
+      >>= fun owner ->
+      match String.cut ~sep:"/" path with
+      | None -> Error (uri_error uri)
+      | Some (repo, "") -> Ok (owner, repo, Fpath.v ".")
+      | Some (repo, path) ->
+          (Fpath.of_string path >>| fun p -> owner, repo, Fpath.rem_empty_seg p)
+          |> R.reword_error_msg (fun _ -> uri_error uri)
+
 let publish_msg p = match p.publish_msg with
 | Some msg -> Ok msg
 | None ->
@@ -355,58 +407,6 @@ let lint_file_with_cmd file_kind ~cmd file errs handle_exit =
         errs + 1
   end
   |> Logs.on_error_msg ~use:(fun () -> errs + 1)
-
-let distrib_owner_and_repo p =
-  distrib_uri p >>= fun uri ->
-  let uri_error uri =
-    R.msgf "Could not derive owner and repo from opam dev-repo \
-            field value %a; expected the pattern \
-            $SCHEME://$HOST/$OWNER/$REPO[.$EXT][/$DIR]" String.dump uri
-  in
-  match Text.split_uri ~rel:true uri with
-  | None -> Error (uri_error uri)
-  | Some (_, _, path) ->
-      if path = "" then Error (uri_error uri) else
-      match String.cut ~sep:"/" path with
-      | None -> Error (uri_error uri)
-      | Some (owner, path) ->
-          let repo = match String.cut ~sep:"/" path with
-          | None -> path
-          | Some (repo, _) -> repo
-          in
-          begin
-            Fpath.of_string repo
-            >>= fun repo -> Ok (owner, Fpath.(to_string @@ rem_ext repo))
-          end
-          |> R.reword_error_msg (fun _ -> uri_error uri)
-
-let doc_uri p = opam_field_hd p "doc" >>| function
-  | None     -> ""
-  | Some uri -> uri
-
-let doc_owner_repo_and_path p =
-  doc_uri p >>= fun uri ->
-  (* Parses the $PATH of $SCHEME://$HOST/$REPO/$PATH *)
-  let uri_error uri =
-    R.msgf "Could not derive publication directory $PATH from opam doc \
-            field value %a; expected the pattern \
-            $SCHEME://$OWNER.github.io/$REPO/$PATH" String.dump uri
-  in
-  match Text.split_uri ~rel:true uri with
-  | None -> Error (uri_error uri)
-  | Some (_, host, path) ->
-      if path = "" then Error (uri_error uri) else
-      (match String.cut ~sep:"." host with
-      | Some (owner, g) when String.equal g "github.io" -> Ok owner
-      | _ -> Error (uri_error uri))
-      >>= fun owner ->
-      match String.cut ~sep:"/" path with
-      | None -> Error (uri_error uri)
-      | Some (repo, "") -> Ok (owner, repo, Fpath.v ".")
-      | Some (repo, path) ->
-          (Fpath.of_string path >>| fun p -> owner, repo, Fpath.rem_empty_seg p)
-          |> R.reword_error_msg (fun _ -> uri_error uri)
-
 
 let lint_opams p =
   Logs.on_error_msg ~use:(fun () -> 1) (
