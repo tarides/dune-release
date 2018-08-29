5c43f93bbada153cb4ac0c6690b893c5e5f9bf64
Nathan Rebours <nathan@cryptosense.com>
2018-08-29 18:48:32 +0000

    Add tests for github URI parsing

---
+++ tests/test_github.ml
@@ -0,0 +1,23 @@
+let user_from_remote_test_case =
+  let test () =
+    let check repo_uri expected =
+      Alcotest.(check (option string))
+        repo_uri
+        expected
+        (Dune_release.Github.user_from_remote repo_uri)
+    in
+    check "git@github.com:username/repo.git" (Some "username");
+    check "git@github.com:user-name/repo.git" (Some "user-name");
+    check "git@github.com:user-name-123/repo.git" (Some "user-name-123");
+    check "git@github.com:123/repo.git" (Some "123");
+    check "wrong" None;
+    check "https://github.com/username/repo.git" None;
+    ()
+  in
+  ("user_from_remote", `Quick, test)
+
+let test_set =
+  ( "Github"
+  , [ user_from_remote_test_case
+    ]
+  )
