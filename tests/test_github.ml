df71d9ba5dcf6064bde6fe2616884b26e3222519
Jeremie Dimino <jeremie@dimino.org>
2018-09-10 11:29:31 +0100

    Fix github url regexp
    Accept urls without .git at the end

--- tests/test_github.ml
+++ tests/test_github.ml
@@ -10,6 +10,11 @@ let user_from_remote_test_case =
     check "git@github.com:user-name/repo.git" (Some "user-name");
     check "git@github.com:user-name-123/repo.git" (Some "user-name-123");
     check "git@github.com:123/repo.git" (Some "123");
+    (* Same as above but without the .git part *)
+    check "git@github.com:username/repo" (Some "username");
+    check "git@github.com:user-name/repo" (Some "user-name");
+    check "git@github.com:user-name-123/repo" (Some "user-name-123");
+    check "git@github.com:123/repo" (Some "123");
     check "wrong" None;
     check "https://github.com/username/repo.git" None;
     ()
