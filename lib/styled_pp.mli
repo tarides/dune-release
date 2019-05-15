4c985d418c7f99050cf13abe1b9e690759bb86e6
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-05-15 15:53:07 +0200

    Move Styled_pp to Text.Pp

--- lib/styled_pp.mli
+++
@@ -1,7 +0,0 @@
-val tag : string Fmt.t
-
-val url : string Fmt.t
-
-val archive : Fpath.t Fmt.t
-
-val package : string Fmt.t
--- lib/text.ml
+++ lib/text.ml
@@ -161,6 +161,7 @@ module Pp = struct
   let commit = Fmt.(styled `Yellow string)
   let dirty = Fmt.(styled_unit `Red "dirty")
   let path = Fmt.(styled `Bold Fpath.pp)
+  let url = Fmt.(styled `Underline string)
   let status ppf = function
   | `Ok -> Fmt.(brackets @@ styled_unit `Green " OK ") ppf ()
   | `Fail -> Fmt.(brackets @@ styled_unit `Red "FAIL") ppf ()
