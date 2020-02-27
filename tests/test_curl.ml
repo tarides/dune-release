ec8bc00e0116407f6cfacb16726b6003d020e1ba
Guillaume Petiot <guillpetiot@gmail.com>
2020-02-27 21:13:54 +0700

    add unit test for Curl.upload_archive

--- tests/test_curl.ml
+++ tests/test_curl.ml
@@ -19,4 +19,28 @@ let test_create_release =
         ];
   ]
 
-let suite = ("Curl", test_create_release)
+let test_upload_archive =
+  let make_test ~test_name ~archive ~user ~repo ~release_id ~expected =
+    let test_fun () =
+      let archive = Fpath.v archive in
+      let actual =
+        Dune_release.Curl.upload_archive ~archive ~user ~repo ~release_id
+      in
+      Alcotest.(check (list string)) test_name expected actual
+    in
+    (test_name, `Quick, test_fun)
+  in
+  [
+    make_test ~test_name:"simple" ~archive:"foo.tgz" ~user:"you"
+      ~repo:"some-repo" ~release_id:27
+      ~expected:
+        [
+          "-H";
+          "Content-Type:application/x-tar";
+          "--data-binary";
+          "@foo.tgz";
+          "https://uploads.github.com/repos/you/some-repo/releases/27/assets?name=foo.tgz";
+        ];
+  ]
+
+let suite = ("Curl", test_create_release @ test_upload_archive)
