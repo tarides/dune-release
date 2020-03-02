ba6641274171bf557ade702fd6614da0b6244d81
Guillaume Petiot <guillpetiot@gmail.com>
2020-03-02 23:03:17 +0700

    Split unit tests of tag functions

--- tests/test_tags.ml
+++ tests/test_tags.ml
@@ -8,70 +8,77 @@ let run f =
   | Error (`Msg e) -> Alcotest.failf "Got an error: %s" e
   | Ok x -> x
 
-let check f ?version ?tag ?keep_v ?opam ~name x =
-  run
-    ( ( match opam with
-      | None -> Ok None
-      | Some lines ->
-          let file = Fpath.(v "opam-tmp") in
-          let lines = ("opam-version", "1.2") :: lines in
-          let lines = List.map (fun (k, v) -> Fmt.strf "%s: %S" k v) lines in
-          Bos.OS.File.write_lines file lines >>| fun () -> Some file )
-    >>= fun opam ->
-      let p = Pkg.v ~dry_run:false ~name ?tag ?version ?keep_v ?opam () in
-      let n = Fmt.strf "check %S" x in
-      f p >>| fun f -> Alcotest.(check fpath) n f Fpath.(v x) )
+let check f ?version ?tag ?keep_v ?opam ~cat ~name x =
+  let test () =
+    run
+      ( ( match opam with
+        | None -> Ok None
+        | Some lines ->
+            let file = Fpath.(v "opam-tmp") in
+            let lines = ("opam-version", "1.2") :: lines in
+            let lines = List.map (fun (k, v) -> Fmt.strf "%s: %S" k v) lines in
+            Bos.OS.File.write_lines file lines >>| fun () -> Some file )
+      >>= fun opam ->
+        let p = Pkg.v ~dry_run:false ~name ?tag ?version ?keep_v ?opam () in
+        let n = Fmt.strf "check %S" x in
+        f p >>| fun f -> Alcotest.(check fpath) n f Fpath.(v x) )
+  in
+  (cat, `Quick, test)
 
-let distrib_file () =
-  let check = check ~name:"foo" Pkg.(distrib_file ~dry_run:true) in
-  check ~tag:"v0" "_build/foo-v0.tbz";
-  check ~version:"v0" "_build/foo-v0.tbz";
-  check ~tag:"v0" ~keep_v:false "_build/foo-v0.tbz";
-  check ~tag:"v0" ~keep_v:true "_build/foo-v0.tbz";
-  check ~tag:"v0" ~version:"x" "_build/foo-v0.tbz"
+let distrib_file =
+  let check =
+    check ~cat:"distrib_file" ~name:"foo" Pkg.(distrib_file ~dry_run:true)
+  in
+  [
+    check ~tag:"v0" "_build/foo-v0.tbz";
+    check ~version:"v0" "_build/foo-v0.tbz";
+    check ~tag:"v0" ~keep_v:false "_build/foo-v0.tbz";
+    check ~tag:"v0" ~keep_v:true "_build/foo-v0.tbz";
+    check ~tag:"v0" ~version:"x" "_build/foo-v0.tbz";
+  ]
 
-let distrib_filename () =
-  let check_true = check ~name:"foo" Pkg.(distrib_filename ~opam:true) in
-  let check_false = check ~name:"foo" Pkg.(distrib_filename ~opam:false) in
-  check_false ~tag:"v0" "foo-v0";
-  check_true ~tag:"v0" "foo.0";
-  check_false ~version:"v0" "foo-v0";
-  check_true ~version:"v0" "foo.v0";
-  check_false ~tag:"v0" ~keep_v:false "foo-v0";
-  check_true ~tag:"v0" ~keep_v:false "foo.0";
-  check_false ~tag:"v0" ~keep_v:true "foo-v0";
-  check_true ~tag:"v0" ~keep_v:true "foo.v0";
-  check_false ~tag:"v0" ~version:"x" "foo-v0";
-  check_true ~tag:"v0" ~version:"x" "foo.x"
+let distrib_filename =
+  let cat = "distrib_filename" in
+  let check_true = check ~cat ~name:"foo" Pkg.(distrib_filename ~opam:true) in
+  let check_false = check ~cat ~name:"foo" Pkg.(distrib_filename ~opam:false) in
+  [
+    check_false ~tag:"v0" "foo-v0";
+    check_true ~tag:"v0" "foo.0";
+    check_false ~version:"v0" "foo-v0";
+    check_true ~version:"v0" "foo.v0";
+    check_false ~tag:"v0" ~keep_v:false "foo-v0";
+    check_true ~tag:"v0" ~keep_v:false "foo.0";
+    check_false ~tag:"v0" ~keep_v:true "foo-v0";
+    check_true ~tag:"v0" ~keep_v:true "foo.v0";
+    check_false ~tag:"v0" ~version:"x" "foo-v0";
+    check_true ~tag:"v0" ~version:"x" "foo.x";
+  ]
 
-let distrib_uri () =
-  let check = check ~name:"yo" (fun x -> Pkg.distrib_uri x >>| Fpath.v) in
+let distrib_uri =
+  let cat = "distrib_uri" in
+  let check = check ~cat ~name:"yo" (fun x -> Pkg.distrib_uri x >>| Fpath.v) in
   let dev_repo = [ ("dev-repo", "git@github.com:foo/bar.git") ] in
   let homepage = [ ("homepage", "https://github.com/foo/bar") ] in
-  check ~opam:dev_repo ~tag:"v0"
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check ~opam:homepage ~tag:"v0"
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check ~opam:dev_repo ~version:"v0"
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check ~opam:homepage ~version:"v0"
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check ~opam:dev_repo ~tag:"v0" ~keep_v:false
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check ~opam:homepage ~tag:"v0" ~keep_v:true
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check ~opam:dev_repo ~tag:"v0" ~version:"x"
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check ~opam:homepage ~tag:"v0" ~version:"x"
-    "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
-  check
-    ~opam:[ ("homepage", "https://foo.github.io/bar") ]
-    ~tag:"v0" "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz"
+  [
+    check ~opam:dev_repo ~tag:"v0"
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check ~opam:homepage ~tag:"v0"
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check ~opam:dev_repo ~version:"v0"
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check ~opam:homepage ~version:"v0"
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check ~opam:dev_repo ~tag:"v0" ~keep_v:false
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check ~opam:homepage ~tag:"v0" ~keep_v:true
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check ~opam:dev_repo ~tag:"v0" ~version:"x"
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check ~opam:homepage ~tag:"v0" ~version:"x"
+      "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+    check
+      ~opam:[ ("homepage", "https://foo.github.io/bar") ]
+      ~tag:"v0" "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz";
+  ]
 
-let suite : unit Alcotest.test =
-  ( "tags",
-    [
-      ("distrib_file", `Quick, distrib_file);
-      ("distrib_filename", `Quick, distrib_filename);
-      ("distrib_uri", `Quick, distrib_uri);
-    ] )
+let suite = ("tags", distrib_file @ distrib_filename @ distrib_uri)
