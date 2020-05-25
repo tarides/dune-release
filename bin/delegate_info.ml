8b82208b7551c7849f5ead89051e3b9431201c55
Sonja Heinze <sonjaleaheinze@gmail.com>
2020-05-25 18:49:57 +0200

    Make error log format consistent
    Before, the error logs in `delegate-info` and `config` were handled
    manually, while in all other commands they were handled through
    `Cli.handle_error`. Now, they all get handled through `Cli.handle_error`.

--- bin/delegate_info.ml
+++ bin/delegate_info.ml
@@ -3,22 +3,19 @@ open Dune_release
 
 let run var =
   let open Rresult in
-  let pkg = Pkg.v ~dry_run:false () in
-  let result =
-    match var with
-    | "tarball" ->
-        Pkg.distrib_archive_path pkg >>| fun distrib_file ->
-        Format.printf "%a\n" Fpath.pp distrib_file
-    | "docdir" -> Ok (Format.printf "%a\n" Fpath.pp Pkg.doc_dir)
-    | "publication-message" ->
-        Pkg.publish_msg pkg >>| fun msg -> Format.printf "%s\n" msg
-    | _ -> Rresult.R.error_msgf "Unknown variable %S" var
-  in
-  match result with
-  | Ok _ -> 0
-  | Error (`Msg msg) ->
-      App_log.unhappy (fun m -> m "%s" msg);
-      1
+  (let pkg = Pkg.v ~dry_run:false () in
+   let result =
+     match var with
+     | "tarball" ->
+         Pkg.distrib_archive_path pkg >>| fun distrib_file ->
+         Format.printf "%a\n" Fpath.pp distrib_file
+     | "docdir" -> Ok (Format.printf "%a\n" Fpath.pp Pkg.doc_dir)
+     | "publication-message" ->
+         Pkg.publish_msg pkg >>| fun msg -> Format.printf "%s\n" msg
+     | _ -> Rresult.R.error_msgf "Unknown variable %S" var
+   in
+   result >>= fun () -> Ok 0)
+  |> Cli.handle_error
 
 let var =
   let doc = "The variable to print." in
