770c4505ef8bf8e3d2a476a143fde687a6f1e209
Guillaume Petiot <guillaume@tarides.com>
2020-10-20 22:28:51 +0700

    Commit and push the update opam files

--- bin/undraft.ml
+++ bin/undraft.ml
@@ -5,6 +5,14 @@ let get_pkg_dir pkg =
   Pkg.build_dir pkg >>= fun bdir ->
   Pkg.distrib_filename ~opam:true pkg >>= fun fname -> Ok Fpath.(bdir // fname)
 
+let pp_opam_repo fmt opam_repo =
+  let user, repo = opam_repo in
+  Format.fprintf fmt "%s/%s" user repo
+
+module D = struct
+  let fetch_head = "${fetch_head}"
+end
+
 let update_opam_file ~dry_run ~url pkg =
   get_pkg_dir pkg >>= fun dir ->
   Pkg.opam pkg >>= fun opam_f ->
@@ -40,12 +48,22 @@ let update_opam_file ~dry_run ~url pkg =
       m "Wrote opam package description %a" Text.Pp.path dest_opam_file)
 
 let undraft ?opam ~name ?distrib_uri ?distrib_file ?opam_repo ?user ?token
-    ?local_repo ?remote_repo ~dry_run ~yes:_ () =
+    ?local_repo ?remote_repo ?pkg_names ~dry_run ~yes:_ () =
   let pkg = Pkg.v ?name ?opam ?distrib_file ~dry_run:false () in
+  Pkg.name pkg >>= fun pkg_name ->
+  let pkg_names = match pkg_names with Some x -> x | None -> [] in
+  let pkg_names = pkg_name :: pkg_names in
   let opam_repo =
     match opam_repo with None -> ("ocaml", "opam-repository") | Some r -> r
   in
   Config.v ~user ~local_repo ~remote_repo [ pkg ] >>= fun config ->
+  ( match local_repo with
+  | Some r -> Ok Fpath.(v r)
+  | None -> (
+      match config.local with
+      | Some r -> Ok r
+      | None -> R.error_msg "Unknown local repository." ) )
+  >>= fun local_repo ->
   ( match remote_repo with
   | Some r -> Ok r
   | None -> (
@@ -76,6 +94,47 @@ let undraft ?opam ~name ?distrib_uri ?distrib_file ?opam_repo ?user ?token
   App_log.status (fun l -> l "Undrafting pull request");
   Sos.Draft_pr.get ~dry_run >>= fun pr_id ->
   update_opam_file ~dry_run ~url pkg >>= fun () ->
+  App_log.status (fun l ->
+      l "Preparing pull request to %a" pp_opam_repo opam_repo);
+  Pkg.version pkg >>= fun version ->
+  let branch = Fmt.strf "release-%s-%s" pkg_name version in
+  Sos.with_dir ~dry_run local_repo
+    (fun () ->
+      let upstream =
+        let user, repo = opam_repo in
+        Printf.sprintf "https://github.com/%s/%s.git" user repo
+      in
+      let remote_branch = "master" in
+      Vcs.get () >>= fun vcs ->
+      App_log.status (fun l ->
+          l "Fetching %a" Text.Pp.url (upstream ^ "#" ^ remote_branch));
+      Vcs.run_git_quiet vcs ~dry_run ~force:true
+        Cmd.(v "fetch" % upstream % remote_branch)
+      >>= fun () ->
+      Vcs.run_git_string vcs ~dry_run ~force:true
+        ~default:(Sos.out D.fetch_head)
+        Cmd.(v "rev-parse" % "FETCH_HEAD")
+      >>= fun id ->
+      Vcs.checkout vcs ~dry_run:false ~branch ~commit_ish:id >>= fun () ->
+      let prepare_package name =
+        let dir = name ^ "." ^ version in
+        let dst = Fpath.(v "packages" / name / dir) in
+        Vcs.run_git_quiet vcs ~dry_run ~force:true Cmd.(v "add" % p dst)
+      in
+      let rec prepare_packages = function
+        | [] -> Ok ()
+        | h :: t -> prepare_package h >>= fun () -> prepare_packages t
+      in
+      prepare_packages pkg_names >>= fun () ->
+      let msg = "Undraft pull-request" in
+      Vcs.run_git_quiet vcs ~dry_run Cmd.(v "commit" % "-m" % msg) >>= fun () ->
+      App_log.status (fun l ->
+          l "Pushing %a to %a" Text.Pp.commit branch Text.Pp.url remote_repo);
+      Vcs.run_git_quiet vcs ~dry_run
+        Cmd.(v "push" % "--force" % remote_repo % branch))
+    ()
+  |> R.join
+  >>= fun () ->
   Github.undraft_pr ~token ~dry_run ~distrib_user ~opam_repo ~pr_id
   >>= fun url ->
   Sos.Draft_release.unset ~dry_run >>= fun () ->
@@ -85,10 +144,10 @@ let undraft ?opam ~name ?distrib_uri ?distrib_file ?opam_repo ?user ?token
 
 let undraft_cli () (`Dist_name name) (`Dist_uri distrib_uri) (`Dist_opam opam)
     (`Dist_file distrib_file) (`Opam_repo opam_repo) (`User user) (`Token token)
-    (`Local_repo local_repo) (`Remote_repo remote_repo) (`Dry_run dry_run)
-    (`Yes yes) =
+    (`Local_repo local_repo) (`Remote_repo remote_repo)
+    (`Package_names pkg_names) (`Dry_run dry_run) (`Yes yes) =
   undraft ?opam ~name ?distrib_uri ?distrib_file ?opam_repo ?user ?token
-    ?local_repo ?remote_repo ~dry_run ~yes ()
+    ?local_repo ?remote_repo ~pkg_names ~dry_run ~yes ()
   |> Cli.handle_error
 
 (* Command line interface *)
@@ -172,5 +231,5 @@ let cmd =
   ( Term.(
       pure undraft_cli $ Cli.setup $ Cli.dist_name $ Cli.dist_uri
       $ Cli.dist_opam $ Cli.dist_file $ opam_repo $ user $ Cli.token
-      $ local_repo $ remote_repo $ Cli.dry_run $ Cli.yes),
+      $ local_repo $ remote_repo $ Cli.pkg_names $ Cli.dry_run $ Cli.yes),
     Term.info "undraft" ~doc ~sdocs ~exits ~envs ~man ~man_xrefs )
