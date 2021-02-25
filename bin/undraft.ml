11cfa00fcf8bbe3da498f4e0caa7cd67f03daea7
Guillaume Petiot <guillaume@tarides.com>
2021-02-25 23:40:43 +0800

    Merge according to #348 and #349, use Pkg.infer_repo_uri and Uri.Github.get_user_and_repo

--- bin/undraft.ml
+++ bin/undraft.ml
@@ -70,10 +70,8 @@ let undraft ?opam ?distrib_uri ?distrib_file ?opam_repo ?user ?token ?local_repo
       | Some r -> Ok r
       | None -> R.error_msg "Unknown remote repository."))
   >>= fun remote_repo ->
-  (match distrib_uri with
-  | Some uri -> Ok uri
-  | None -> Pkg.infer_distrib_uri pkg)
-  >>= Pkg.distrib_user_and_repo
+  (match distrib_uri with Some uri -> Ok uri | None -> Pkg.infer_repo_uri pkg)
+  >>= Uri.Github.get_user_and_repo
   >>= fun (distrib_user, repo) ->
   let user =
     match config.user with
