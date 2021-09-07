dc44ebe4b23e285543882bbd7017f925f6caec68
Marek Kubica <marek@tarides.com>
2021-09-07 15:25:26 +0200

    Give individual tests identifiers

--- tests/lib/test_tags.ml
+++ tests/lib/test_tags.ml
@@ -8,7 +8,7 @@ let run f =
   | Error (`Msg e) -> Alcotest.failf "Got an error: %s" e
   | Ok x -> x
 
-let check f ?version ?tag ?keep_v ?opam ~cat ~name expected =
+let make_test f ?version ?tag ?keep_v ?opam ~cat ~name expected =
   let test () =
     run
       ( (match opam with
@@ -27,52 +27,59 @@ let check f ?version ?tag ?keep_v ?opam ~cat ~name expected =
   (cat, `Quick, test)
 
 let distrib_file =
-  let check =
-    check ~cat:"distrib_file" ~name:"foo" Pkg.(distrib_file ~dry_run:true)
+  let make_test ~test_name =
+    let cat = "distrib_file: " ^ test_name in
+    make_test ~cat ~name:"foo" Pkg.(distrib_file ~dry_run:true)
   in
   [
-    check ~tag:"v0" "_build/foo-v0.tbz";
-    check ~version:"v0" "_build/foo-v0.tbz";
-    check ~tag:"v0" ~keep_v:false "_build/foo-v0.tbz";
-    check ~tag:"v0" ~keep_v:true "_build/foo-v0.tbz";
-    check ~tag:"v0" ~version:"x" "_build/foo-v0.tbz";
+    make_test ~test_name:"tag" ~tag:"v0" "_build/foo-v0.tbz";
+    make_test ~test_name:"version" ~version:"v0" "_build/foo-v0.tbz";
+    make_test ~test_name:"tag without v" ~tag:"v0" ~keep_v:false
+      "_build/foo-v0.tbz";
+    make_test ~test_name:"tag with v" ~tag:"v0" ~keep_v:true "_build/foo-v0.tbz";
+    make_test ~test_name:"tag and version" ~tag:"v0" ~version:"x"
+      "_build/foo-v0.tbz";
   ]
 
 let distrib_filename =
-  let cat = "distrib_filename" in
-  let check_true = check ~cat ~name:"foo" Pkg.(distrib_filename ~opam:true) in
-  let check_false = check ~cat ~name:"foo" Pkg.(distrib_filename ~opam:false) in
+  let make_test ~test_name ~opam =
+    let cat = "distrib_filename: " ^ test_name in
+    make_test ~cat ~name:"foo" (Pkg.distrib_filename ~opam)
+  in
   [
-    check_false ~tag:"v0" "foo-v0";
-    check_true ~tag:"v0" "foo.0";
-    check_false ~version:"v0" "foo-v0";
-    check_true ~version:"v0" "foo.v0";
-    check_false ~tag:"v0" ~keep_v:false "foo-v0";
-    check_true ~tag:"v0" ~keep_v:false "foo.0";
-    check_false ~tag:"v0" ~keep_v:true "foo-v0";
-    check_true ~tag:"v0" ~keep_v:true "foo.v0";
-    check_false ~tag:"v0" ~version:"x" "foo-v0";
-    check_true ~tag:"v0" ~version:"x" "foo.x";
+    make_test ~test_name:"1" ~opam:false ~tag:"v0" "foo-v0";
+    make_test ~test_name:"2" ~opam:true ~tag:"v0" "foo.0";
+    make_test ~test_name:"3" ~opam:false ~version:"v0" "foo-v0";
+    make_test ~test_name:"4" ~opam:true ~version:"v0" "foo.v0";
+    make_test ~test_name:"5" ~opam:false ~tag:"v0" ~keep_v:false "foo-v0";
+    make_test ~test_name:"6" ~opam:true ~tag:"v0" ~keep_v:false "foo.0";
+    make_test ~test_name:"7" ~opam:false ~tag:"v0" ~keep_v:true "foo-v0";
+    make_test ~test_name:"8" ~opam:true ~tag:"v0" ~keep_v:true "foo.v0";
+    make_test ~test_name:"9" ~opam:false ~tag:"v0" ~version:"x" "foo-v0";
+    make_test ~test_name:"10" ~opam:true ~tag:"v0" ~version:"x" "foo.x";
   ]
 
 let distrib_uri =
-  let cat = "distrib_uri" in
-  let check =
-    check ~cat ~name:"yo" (fun x -> Pkg.infer_github_distrib_uri x >>| Fpath.v)
+  let make_test ~test_name =
+    let cat = "distrib_uri:" ^ test_name in
+    make_test ~cat ~name:"yo" (fun x ->
+        Pkg.infer_github_distrib_uri x >>| Fpath.v)
   in
   let dev_repo = [ ("dev-repo", "git@github.com:foo/bar.git") ] in
   let homepage = [ ("homepage", "https://github.com/foo/bar") ] in
   let url = "https://github.com/foo/bar/releases/download/v0/yo-v0.tbz" in
   [
-    check ~opam:dev_repo ~tag:"v0" url;
-    check ~opam:homepage ~tag:"v0" url;
-    check ~opam:dev_repo ~version:"v0" url;
-    check ~opam:homepage ~version:"v0" url;
-    check ~opam:dev_repo ~tag:"v0" ~keep_v:false url;
-    check ~opam:homepage ~tag:"v0" ~keep_v:true url;
-    check ~opam:dev_repo ~tag:"v0" ~version:"x" url;
-    check ~opam:homepage ~tag:"v0" ~version:"x" url;
-    check ~opam:[ ("homepage", "https://foo.github.io/bar") ] ~tag:"v0" url;
+    make_test ~test_name:"1" ~opam:dev_repo ~tag:"v0" url;
+    make_test ~test_name:"2" ~opam:homepage ~tag:"v0" url;
+    make_test ~test_name:"3" ~opam:dev_repo ~version:"v0" url;
+    make_test ~test_name:"4" ~opam:homepage ~version:"v0" url;
+    make_test ~test_name:"5" ~opam:dev_repo ~tag:"v0" ~keep_v:false url;
+    make_test ~test_name:"6" ~opam:homepage ~tag:"v0" ~keep_v:true url;
+    make_test ~test_name:"7" ~opam:dev_repo ~tag:"v0" ~version:"x" url;
+    make_test ~test_name:"8" ~opam:homepage ~tag:"v0" ~version:"x" url;
+    make_test ~test_name:"9"
+      ~opam:[ ("homepage", "https://foo.github.io/bar") ]
+      ~tag:"v0" url;
   ]
 
 let suite = ("tags", distrib_file @ distrib_filename @ distrib_uri)
