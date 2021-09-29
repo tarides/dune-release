c16b97bf0e83304571a51b874562623643cffbba
Marek Kubica <marek@tarides.com>
2021-09-29 15:14:19 +0200

    Remove helper `t` function

--- tests/lib/test_vcs.ml
+++ tests/lib/test_vcs.ml
@@ -5,36 +5,34 @@ let make_test name ~input ~expected ~prefix ~f =
   let test_fun () = Alcotest.(check string) name expected (f input) in
   (name, `Quick, test_fun)
 
-let t = Vcs.Tag.of_string
-
 let test_git_escape_tag =
   let make_test name ~input ~expected =
     let name = Printf.sprintf "git_escape_tag: %s" name in
+    let expected = Vcs.Tag.of_string expected in
     let test_fun () =
       Alcotest.(check Alcotest_ext.tag) name expected (Vcs.git_escape_tag input)
     in
     (name, `Quick, test_fun)
   in
   [
-    make_test "empty" ~input:"" ~expected:(t "");
-    make_test "valid" ~input:"3.3.4" ~expected:(t "3.3.4");
-    make_test "tilde" ~input:"3.3.4~4.10preview1"
-      ~expected:(t "3.3.4_4.10preview1");
+    make_test "empty" ~input:"" ~expected:"";
+    make_test "valid" ~input:"3.3.4" ~expected:"3.3.4";
+    make_test "tilde" ~input:"3.3.4~4.10preview1" ~expected:"3.3.4_4.10preview1";
   ]
 
 let test_git_unescape_tag =
   let make_test name ~input ~expected =
     let name = Printf.sprintf "git_unescape_tag: %s" name in
+    let input = Vcs.Tag.of_string input in
     let test_fun () =
       Alcotest.(check string) name expected (Vcs.git_unescape_tag input)
     in
     (name, `Quick, test_fun)
   in
   [
-    make_test "empty" ~input:(t "") ~expected:"";
-    make_test "valid" ~input:(t "3.3.4") ~expected:"3.3.4";
-    make_test "tilde" ~input:(t "3.3.4_4.10preview1")
-      ~expected:"3.3.4~4.10preview1";
+    make_test "empty" ~input:"" ~expected:"";
+    make_test "valid" ~input:"3.3.4" ~expected:"3.3.4";
+    make_test "tilde" ~input:"3.3.4_4.10preview1" ~expected:"3.3.4~4.10preview1";
   ]
 
 let suite = ("Vcs", test_git_escape_tag @ test_git_unescape_tag)
