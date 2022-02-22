db800071a175284c1e78951dbf14765933679165
lubegasimon <lubegasimon73@gmail.com>
2022-02-22 17:58:32 +0300

    remove dune-release.bin pkg

--- bin/check.mli
+++ bin/check.mli
@@ -1,3 +1,15 @@
+open Dune_release
+
+(** expose the [check function]. *)
+val check : [< `Package_names of string list ] ->
+    [< `Package_version of Version.t option ] ->
+    [< `Dist_tag of Vcs.Tag.t option ] ->
+    [< `Keep_v of bool Dune_release.Config.Cli.t ] ->
+    [< `Build_dir of Fpath.t option ] ->
+    [< `Skip_lint of bool ] ->
+    [< `Skip_build of bool ] ->
+    [< `Skip_tests of bool ] -> [< `Working_tree of bool ] -> int
+
 (** The [check] command. *)
 
 val cmd : int Cmdliner.Cmd.t
