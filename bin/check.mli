07b115ae71095128736fd9c4395f9c84b73db04c
lubegasimon <lubegasimon73@gmail.com>
2022-03-09 11:44:49 +0300

    revert exposing check function
    Signed-off-by: lubegasimon <lubegasimon73@gmail.com>

--- bin/check.mli
+++ bin/check.mli
@@ -1,18 +1,3 @@
-open Dune_release
-
-val check :
-  [< `Package_names of string list ] ->
-  [< `Package_version of Version.t option ] ->
-  [< `Dist_tag of Vcs.Tag.t option ] ->
-  [< `Keep_v of bool Dune_release.Config.Cli.t ] ->
-  [< `Build_dir of Fpath.t option ] ->
-  [< `Skip_lint of bool ] ->
-  [< `Skip_build of bool ] ->
-  [< `Skip_tests of bool ] ->
-  [< `Working_tree of bool ] ->
-  int
-(** expose the [check function]. *)
-
 (** The [check] command. *)
 
 val cmd : int Cmdliner.Cmd.t
