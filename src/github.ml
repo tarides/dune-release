fce88a170eba5b4bbe27276e363d22815b10666f
Thibaut Mattio <thibaut.mattio@gmail.com>
2023-05-25 12:52:22 +0200

    Handle pagination for projects to get more than 100 items
    Also:
    - Move GraphQL query for projects to `Project`
    - Move GitHub API handling to a `Github` module
    - Add basic caching to HTTP requests

---
+++ src/github.ml
@@ -0,0 +1,60 @@
+module Token = struct
+  let github_token_env () = Sys.getenv_opt "GITHUB_TOKEN"
+
+  let github_token_file () =
+    let f = Sys.getenv "HOME" ^ "/.github/github-activity-token" in
+    let ic = open_in f in
+    let n = in_channel_length ic in
+    String.trim (really_input_string ic n)
+
+  let t =
+    match github_token_env () with Some x -> x | None -> github_token_file ()
+end
+
+module U = Yojson.Safe.Util
+
+let ( / ) a b = U.member b a
+let cache = Hashtbl.create 128
+let graphql_endpoint = "https://api.github.com/graphql"
+
+let debug =
+  match Sys.getenv "GH_DEBUG" with
+  | "" | "0" | "false" -> false
+  | _ -> true
+  | exception Not_found -> false
+
+let run query =
+  if Hashtbl.mem cache query then Lwt.return (Hashtbl.find cache query)
+  else
+    let open Lwt.Syntax in
+    if debug then Fmt.epr "QUERY: %s\n" query;
+    let body =
+      `Assoc [ ("query", `String query) ]
+      |> Yojson.Safe.to_string |> Cohttp_lwt.Body.of_string
+    in
+    let headers =
+      Cohttp.Header.init_with "Authorization" ("bearer " ^ Token.t)
+    in
+    let* resp, body =
+      Cohttp_lwt_unix.Client.post ~headers ~body
+        (Uri.of_string graphql_endpoint)
+    in
+    let+ body = Cohttp_lwt.Body.to_string body in
+    let response =
+      match Cohttp.Response.status resp with
+      | `OK -> (
+          let json = Yojson.Safe.from_string body in
+          match json / "errors" with
+          | `Null -> json
+          | _errors ->
+              Fmt.failwith "@[<v2>GitHub returned errors: %a@]"
+                (Yojson.Safe.pretty_print ~std:true)
+                json)
+      | err ->
+          Fmt.failwith
+            "@[<v2>Error performing GraphQL query on GitHub: %s@,%s@]"
+            (Cohttp.Code.string_of_status err)
+            body
+    in
+    Hashtbl.add cache query response;
+    response
--- src/project.ml
+++ src/project.ml
@@ -1,8 +1,66 @@
-module U = Yojson.Safe.Util
-
-let ( / ) a b = U.member b a
-
 type t = { id : string; title : string; cards : Card.t list }
+
+module Query = struct
+  let make ~org_name ~project_number ~after =
+    Printf.sprintf
+      {|
+query { 
+  organization(login: %S) {
+    projectV2(number: %d) {
+      id
+      title
+      items(first: 100 %s) {
+        edges {
+          cursor
+          node {
+            fieldValues(first: 10) {
+              nodes {
+                ... on ProjectV2ItemFieldTextValue {
+                  text
+                  field {
+                    ... on ProjectV2FieldCommon {
+                      name
+                    }
+                  }
+                }
+                ... on ProjectV2ItemFieldSingleSelectValue {
+                  name
+                  field {
+                    ... on ProjectV2FieldCommon {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+|}
+      org_name project_number
+      (match after with
+      | None -> ""
+      | Some s -> Printf.sprintf ", after: \"%s\" " s)
+
+  module U = Yojson.Safe.Util
+
+  let ( / ) a b = U.member b a
+
+  let parse json =
+    let json = json / "data" / "organization" / "projectV2" in
+    let id = json / "id" |> U.to_string in
+    let title = json / "title" |> U.to_string in
+    let edges = json / "items" / "edges" |> U.to_list in
+    match edges with
+    | [] -> ("", { id; title; cards = [] })
+    | edges ->
+        let cursor = (List.rev edges |> List.hd) / "cursor" |> U.to_string in
+        let cards = List.map (fun edge -> edge / "node" |> Card.parse) edges in
+        (cursor, { id; title; cards })
+end
 
 let filter ?(filter_out = Filter.default_out) data =
   { data with cards = Card.filter_out filter_out data.cards }
@@ -16,28 +74,6 @@ let to_csv t =
   Csv.close_out out;
   Buffer.contents buffer
 
-let graphql project_number =
-  Fmt.str
-    {|
-    projectV2(number: %d) {
-      id
-      title
-      items(first: 100) {
-        nodes {
-          %s
-        }
-      }
-    }
-    |}
-    project_number Card.graphql
-
-let parse json =
-  let json = json / "data" / "organization" / "projectV2" in
-  let id = json / "id" |> U.to_string in
-  let title = json / "title" |> U.to_string in
-  let cards = json / "items" / "nodes" |> U.to_list |> List.map Card.parse in
-  { id; title; cards }
-
 let pp ?(order_by = Column.Objective) ?(filter_out = Filter.default_out) ppf t =
   Fmt.pf ppf "\n== %s (%s) ==\n" t.title t.id;
   let sections = Card.filter_out filter_out t.cards in
@@ -50,3 +86,20 @@ let pp ?(order_by = Column.Objective) ?(filter_out = Filter.default_out) ppf t =
         Fmt.pf ppf "\n - %s -\n" name;
         List.iter (Card.pp ppf) section)
       sections
+
+let get ~org_name ~project_number =
+  let open Lwt.Syntax in
+  let rec aux cursor acc =
+    let query = Query.make ~org_name ~project_number ~after:cursor in
+    let* json = Github.run query in
+    let cursor, project = Query.parse json in
+    if List.length project.cards < 100 then
+      Lwt.return { project with cards = acc @ project.cards }
+    else aux (Some cursor) (acc @ project.cards)
+  in
+  aux None []
+
+let get_all ~org_name project_numbers =
+  Lwt_list.map_p
+    (fun project_number -> get ~org_name ~project_number)
+    project_numbers
