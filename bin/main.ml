cbfe1d76a954254762948ec43949072aac0c42e1
Thomas Gazagnaire <thomas@gazagnaire.org>
2023-07-10 19:39:13 +0200

    Use -v and -vv to set verbosity

--- bin/main.ml
+++ bin/main.ml
@@ -169,8 +169,11 @@ let timesheets_term =
 let setup =
   let style_renderer = Fmt_cli.style_renderer ~docs:common_options () in
   Term.(
-    const (fun style_renderer -> Fmt_tty.setup_std_outputs ?style_renderer ())
-    $ style_renderer)
+    const (fun style_renderer level ->
+        Fmt_tty.setup_std_outputs ?style_renderer ();
+        Logs.set_level level;
+        Logs.set_reporter (Logs_fmt.reporter ()))
+    $ style_renderer $ Logs_cli.level ())
 
 let filter_sync = [ (Column.Id, Filter.is "New KR"); (Id, Filter.is "") ]
 
