5f12e21dba80057b51bb398edbe857be7ebfdea5
Guillaume "Liam" Petiot <guillaume@tarides.com>
2024-06-06 16:25:57 +0100

    Re-use Get_activity.Graphql.request and exec in Github.run (#70)

--- lib/github.ml
+++ lib/github.ml
@@ -21,9 +21,7 @@ end
 
 module U = Yojson.Safe.Util
 
-let ( / ) a b = U.member b a
 let cache = Hashtbl.create 128
-let graphql_endpoint = "https://api.github.com/graphql"
 
 let debug =
   match Sys.getenv "GH_DEBUG" with
@@ -34,36 +32,16 @@ let debug =
 let run query =
   Fmt.pr "Querying Github...\n%!";
   if Hashtbl.mem cache query then Lwt.return (Hashtbl.find cache query)
-  else
-    let open Lwt.Syntax in
+  else (
     if debug then Fmt.epr "QUERY: %s\n%!" query;
-    let body =
-      `Assoc [ ("query", `String query) ]
-      |> Yojson.Safe.to_string |> Cohttp_lwt.Body.of_string
-    in
-    let headers =
-      Cohttp.Header.init_with "Authorization" ("bearer " ^ Lazy.force Token.t)
-    in
-    let* resp, body =
-      Cohttp_lwt_unix.Client.post ~headers ~body
-        (Uri.of_string graphql_endpoint)
-    in
-    let+ body = Cohttp_lwt.Body.to_string body in
+    let token = Lazy.force Token.t in
+    let request = Get_activity.Graphql.request ~token ~query () in
     let response =
-      match Cohttp.Response.status resp with
-      | `OK -> (
-          let json = Yojson.Safe.from_string body in
-          match json / "errors" with
-          | `Null -> json
-          | _errors ->
-              Fmt.failwith "@[<v2>GitHub returned errors: %a@]"
-                (Yojson.Safe.pretty_print ~std:true)
-                json)
-      | err ->
-          Fmt.failwith
-            "@[<v2>Error performing GraphQL query on GitHub: %s@,%s@]"
-            (Cohttp.Code.string_of_status err)
-            body
+      match Get_activity.Graphql.exec request with
+      | Ok resp -> resp
+      | Error (`Msg msg) ->
+          Fmt.failwith "@[<v2>Error performing GraphQL query on GitHub: %s@]"
+            msg
     in
     Hashtbl.add cache query response;
-    response
+    Lwt.return response)
