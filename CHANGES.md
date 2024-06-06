25c4fde243c9013fab9f777231c0ff6e36d3565f
Guillaume "Liam" Petiot <guillaume@tarides.com>
2024-06-06 15:05:45 +0100

    Fetch: fix the source for timesheets when okr-update-dir or admin-dir is specified (#69)

--- CHANGES.md
+++ CHANGES.md
@@ -4,4 +4,8 @@
 
 - Depend on okra-lib.2.0.0 (#56, #65, #68, @gpetiot)
 
+### Fixed
+
+- Fetch: fix the source for timesheets when okr-update-dir or admin-dir is specified (#69 , @gpetiot)
+
 (changes before May '24 not tracked)
--- bin/fetch.ml
+++ bin/fetch.ml
@@ -11,38 +11,38 @@ let write_timesheets ~dir t =
   let data = Fmt.str "%a" pp_csv_ts t in
   IO.write_file file data
 
+let fetch_timesheets ~dir t =
+  match t.source with
+  | Local | Github -> ()
+  | Okr_updates | Admin ->
+      let report = IO.get_timesheets ~lint:false t in
+      write_timesheets ~dir report
+
+let fetch_project_board ~dir t =
+  match t.source with
+  | Github ->
+      let+ project = IO.get_project t in
+      IO.write ~dir project
+  | Local | Okr_updates | Admin -> Lwt.return ()
+
 let run ({ Common.data_dir; source; okr_updates_dir; admin_dir; _ } as t) =
   Lwt_main.run
   @@
-  let () =
-    let source =
-      match (source, okr_updates_dir, admin_dir) with
-      | Github, Some dir, _ ->
-          Fmt.epr "Reading timesheets from `%s'.\n%!" dir;
-          Okr_updates
-      | Github, _, Some dir ->
-          Fmt.epr "Reading timesheets from `%s'.\n%!" dir;
-          Admin
-      | Github, _, _ ->
-          Fmt.epr "Skipping timesheets.\n%!";
-          source
-      | _ -> source
-    in
-    (* fetch timesheets *)
-    match source with
-    | Local | Github -> ()
-    | _ ->
-        let report = IO.get_timesheets ~lint:false t in
-        write_timesheets ~dir:data_dir report
+  let source =
+    match (source, okr_updates_dir, admin_dir) with
+    | Github, Some dir, _ ->
+        Fmt.epr "Reading timesheets from `%s'.\n%!" dir;
+        Okr_updates
+    | Github, _, Some dir ->
+        Fmt.epr "Reading timesheets from `%s'.\n%!" dir;
+        Admin
+    | (Github | Local), _, _ ->
+        Fmt.epr "Skipping timesheets.\n%!";
+        source
+    | _ -> source
   in
-  let+ () =
-    (* fetch project boards *)
-    match source with
-    | Github ->
-        let+ project = IO.get_project t in
-        IO.write ~dir:data_dir project
-    | _ -> Lwt.return ()
-  in
+  let () = fetch_timesheets ~dir:data_dir { t with source } in
+  let+ () = fetch_project_board ~dir:data_dir t in
   ()
 
 let cmd =
